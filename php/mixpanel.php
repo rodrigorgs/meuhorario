9050c8621c623fdf70d402d702eec28f31f8f215
Gabriel Erbetta <gabrielerbetta@gmail.com>
2016-06-10 08:52:19 -0300

    Deixa compatível com PHP 5.3

--- php/mixpanel.php
+++ php/mixpanel.php
@@ -46,14 +46,14 @@ function track_gerou_horario($request, $curso) {
   $cod_curso = $request['curso'];
   $selected = array_keys($request, 'on');
 
-  $cod_materias = [];
-  $nome_materias = [];
-  $cod_turmas = [];
+  $cod_materias = array();
+  $nome_materias = array();
+  $cod_turmas = array();
   foreach ($curso->listaDeMaterias as $i => $materia) {
     array_push($cod_materias, $materia->codigo);
     array_push($nome_materias, $materia->nome);
 
-    $cod_turmas_materia = [];
+    $cod_turmas_materia = array();
     foreach ($materia->listaDeTurmas as $i => $turma) {
       array_push($cod_turmas_materia, $turma->codigo);
     }
@@ -68,7 +68,7 @@ function track_gerou_horario($request, $curso) {
   $conflitos = in_array('mostraConflito', $selected) ? 'true' : 'false';
   $carrascos = $request['carrascos'] ? '["' . str_replace("\r\n", '","', $request['carrascos']) . '"]' : '';
 
-  $rest_horario = [];
+  $rest_horario = array();
   foreach ($selected as $i => $s) {
     if (in_array($s, $horarios)) array_push($rest_horario, $s);
   }
